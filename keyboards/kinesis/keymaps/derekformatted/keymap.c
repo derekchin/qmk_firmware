e3588c85c67db51bfebaf5ed4e5d1908807b1697
Derek Chin <derekwchin+ssi@gmail.com>
2019-03-01 17:13:05 -0800

    Add QWERTY layer

--- keyboards/kinesis/keymaps/derekformatted/keymap.c
+++ keyboards/kinesis/keymaps/derekformatted/keymap.c
@@ -41,12 +41,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,         KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_SLCK,  KC_PAUS,  KC_FN0,   RESET,
     KC_EQL,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                                                                      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                                                                      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
-    KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                                                                      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
+    CTL_T(KC_ESCAPE),  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                                                             KC_H,     KC_J,     KC_K,     KC_L,     LT(2, KC_SCOLON), LT(2, KC_QUOTE),
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                                                                      KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
-              KC_GRV,   KC_INS,   KC_LEFT,  KC_RGHT,                                                                                       KC_UP,    KC_DOWN,  KC_LBRC,  KC_RBRC,
-                                                      KC_LCTL,  KC_LALT,                                               KC_RGUI,  KC_RCTL,
-                                                                KC_HOME,                                               KC_PGUP,
-                                            KC_BSPC,  KC_DEL,   KC_END,                                                KC_PGDN,  KC_ENTER, KC_SPC
+              LT(1, KC_NO), LT(5, KC_NO), ALT_T(KC_F19), LT(4, KC_F20),                                                                       LALT(LCTL(LGUI(LSFT(KC_EQUAL)))), LSFT(KC_RALT), LT(5, KC_NO), KC_MEH, 
+                                                 LT(5, KC_F21), LT(3, KC_F22),                                                LT(2, KC_F17), LT(5, KC_F16),
+                                                              LT(4, KC_F23),                                                KC_F18,
+                            GUI_T(KC_SPACE), LT(3, KC_SPACE), LT(3, KC_F24),                                                KC_F19, KC_BSPACE, LT(5, KC_SPACE)
   ),
 
   [TWO] = LAYOUT_pretty(
