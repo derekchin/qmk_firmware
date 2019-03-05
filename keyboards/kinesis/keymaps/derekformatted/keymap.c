4d4f8d8cdf6ded427faf8eeb46c8182b7b8f0e7b
Derek Chin <derekwchin+ssi@gmail.com>
2019-03-04 21:30:38 -0800

    Clean up HYPR and test of adding steno

--- keyboards/kinesis/keymaps/derekformatted/keymap.c
+++ keyboards/kinesis/keymaps/derekformatted/keymap.c
@@ -42,12 +42,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [QWERTY] = LAYOUT_pretty(
     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,         KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_SLCK,  KC_PAUS,  TG(KINESIS),   RESET,
-    KC_EQL,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                                                                      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
+    KC_EQL,   KC_1,     MT(MOD_LSFT, KC_2),     MT(MOD_LCTL,KC_3),     MT(MOD_LALT,KC_4),     KC_5,    KC_6,     MT(MOD_RALT, KC_7),     MT(MOD_RCTL, KC_8),     MT(MOD_RSFT, KC_9),     KC_0,     KC_MINS,
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                                                                      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
     CTL_T(KC_ESCAPE),  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                                                             KC_H,     KC_J,     KC_K,     KC_L,     LT(2, KC_SCOLON), LT(2, KC_QUOTE),
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                                                                      KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
-              LT(1, KC_TRNS), LT(5, KC_TRNS), ALT_T(KC_F19), LT(4, KC_F20),                                                                       LALT(LCTL(LGUI(LSFT(KC_EQUAL)))), LSFT(KC_RALT), LT(5, KC_TRNS), KC_MEH, 
-                                                 LSFT(KC_LALT), LT(3, KC_F22),                                                LT(2, KC_F17), RSFT(KC_RALT),
+               LT(1, KC_TRNS), LT(5, KC_TRNS), ALT_T(KC_F19), LT(4, KC_F20),                                                                       HYPR(KC_EQUAL), LSFT(KC_RALT), LT(5, KC_TRNS), KC_MEH, 
+                                               LSFT(KC_LALT), LT(3, KC_F22),                                                LT(2, KC_F17), RSFT(KC_RALT),
                                                               LT(4, KC_F23),                                                KC_F18,
                             GUI_T(KC_SPACE), LT(3, KC_SPACE), LT(3, KC_F24),                                                KC_F19, KC_BSPACE, LT(5, KC_SPACE)
   ),
@@ -68,9 +68,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [MOUSE] = LAYOUT_pretty(
     KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,         KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   RESET,
     KC_TRNS,   KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,                                                                     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,
-    KC_TRNS,   KC_TRNS,     LALT(LCTL(LGUI(LSFT(KC_F1)))),KC_MS_UP,LALT(LCTL(LGUI(LSFT(KC_F2)))), KC_TRNS,                                KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,
+    KC_TRNS,   KC_TRNS,     HYPR(KC_F1),KC_MS_UP,HYPR(KC_F2), KC_TRNS,                                KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,     KC_TRNS,
     KC_TRNS,  KC_TRNS,     KC_MS_LEFT,KC_MS_DOWN,KC_MS_RIGHT, KC_TRNS,                                                                    KC_TRNS,     KC_MS_BTN1,KC_MS_BTN2,KC_TRNS,     KC_TRNS,  KC_TRNS,
-    KC_TRNS,  KC_TRNS,     LALT(LCTL(LGUI(LSFT(KC_F3)))),KC_TRANSPARENT,LALT(LCTL(LGUI(LSFT(KC_F4)))), KC_TRNS,                           KC_TRNS,     KC_TRNS,     KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,
+    KC_TRNS,  KC_TRNS,     HYPR(KC_F3),KC_TRANSPARENT,HYPR(KC_F4), KC_TRNS,                           KC_TRNS,     KC_TRNS,     KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,
               KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,                                                                                      KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,
                                                       KC_TRNS,  KC_TRNS,                                               KC_TRNS,  KC_TRNS,
                                                                 KC_TRNS,                                               KC_TRNS,
