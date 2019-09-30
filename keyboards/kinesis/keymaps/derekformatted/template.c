01d7c3c7c8124dc69aea164646a9898ee298b9ad
Derek Chin <derekwchin+ssi@gmail.com>
2019-09-30 01:38:09 -0700

    Add switcher layers

---
+++ keyboards/kinesis/keymaps/derekformatted/template.c
@@ -0,0 +1,24 @@
+
+  [TEMPLATE] = LAYOUT_pretty(
+    KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,  RESET,
+    KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,                            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+    KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,                            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+    KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,                            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+    KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,                            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+                KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,                                        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+                                                  KC_TRNS,  KC_TRNS,                              KC_TRNS,  KC_TRNS,
+                                                            KC_TRNS,                              KC_TRNS,
+                                        KC_TRNS,  KC_TRNS,  KC_TRNS,                              KC_TRNS,  KC_TRNS, KC_TRNS
+  ),
+
+  [TEMPLATE2] = LAYOUT_pretty(
+    KC_NO,    KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_NO,  KC_NO,  RESET,
+    KC_NO,    KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_NO,                            KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
+    KC_NO,    KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_NO,                            KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
+    KC_NO,    KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_NO,                            KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
+    KC_NO,    KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_NO,                            KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
+                KC_NO,   KC_NO,   KC_NO,    KC_NO,                                        KC_NO,  KC_NO,  KC_NO,  KC_NO,
+                                                  KC_NO,  KC_NO,                              KC_NO,  KC_NO,
+                                                            KC_NO,                              KC_NO,
+                                        KC_NO,  KC_NO,  KC_NO,                              KC_NO,  KC_NO, KCNO_
+  ),
