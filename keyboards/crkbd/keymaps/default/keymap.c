0a7aa8f3665f4c74ef3d8ec2076eeaccd3f88f26
Derek Chin <derekchin@secondspectrum.com>
2025-05-02 21:04:37 -0700

    feat: Latest

--- keyboards/crkbd/keymaps/default/keymap.c
+++ keyboards/crkbd/keymaps/default/keymap.c
@@ -36,8 +36,8 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_QWERTY] = LAYOUT_split_3x6_3_ex2(
-    KC_TAB,         KC_Q,   KC_W,               KC_E,               KC_R,               KC_T,           KC_LCTL,    KC_RCTL,    KC_Y,     KC_U,                        KC_I,                       KC_O,               KC_P,                   KC_BSLS,
-    CTL_T(KC_ESC),  KC_A,   KC_S,               KC_D,               KC_F,               KC_G,           TO(_MOUSE),    KC_RCTL,    KC_H,     KC_J,                        KC_K,                       KC_L,               LT(_MOUSE, KC_SCLN),  CTL_T(KC_QUOTE),
+    KC_TAB,         KC_Q,   KC_W,               KC_E,               KC_R,               KC_T,           KC_LCTL,     KC_ENT,    KC_Y,     KC_U,                        KC_I,                       KC_O,               KC_P,                   KC_BSLS,
+    CTL_T(KC_ESC),  KC_A,   KC_S,               KC_D,               KC_F,               KC_G,           TO(_MOUSE),  KC_BSPC,   KC_H,     KC_J,                        KC_K,                       KC_L,               LT(_MOUSE, KC_SCLN),  CTL_T(KC_QUOTE),
     KC_LSFT,        KC_Z,   KC_X,               KC_C,               KC_V,               KC_B,                                   KC_N,     KC_M,                        KC_COMM,                    KC_DOT,             KC_SLSH,                KC_RSFT,
                                     KC_SPACE,      GUI_T(KC_SPACE),    MO(_LAUNCHERL),    LT(_LAUNCHERR,KC_BSPC),    LT(_LAUNCHERL, KC_SPACE),   KC_SPACE
     ),
