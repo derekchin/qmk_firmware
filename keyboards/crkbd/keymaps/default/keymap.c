059b0e08373fd31fe044ea6deeeb83b5abaaf3f6
Derek Chin <derekwchin@gmail.com>
2026-05-02 17:35:05 -0700

    feat: Add fixes for Editor error, and layer freezing

--- keyboards/crkbd/keymaps/default/keymap.c
+++ keyboards/crkbd/keymaps/default/keymap.c
@@ -22,9 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 
 // Defines names for use in layer keycodes and the keymap
-enum layer_names { _QWERTY, _LAUNCHERL, _LAUNCHERR, _NUMBERS, _MOUSE, _ARROWS, _WINDOWS, _SYMBOLSL, _SYMBOLSR, _GAMING };
-
-
+enum layer_names { _QWERTY, _LAUNCHERL, _LAUNCHERR, _NUMBERS, _MOUSE, _ARROWS, _SYMBOLSL, _SYMBOLSR };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -36,21 +34,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_LAUNCHERL] = LAYOUT_split_3x6_3_ex2(
-        _______      , KC_LSFT    , KC_LCTL, KC_LALT     , KC_LCMD      , KC_TAB     , KC_ESC , KC_ENT , _______, _______, _______, _______, _______, _______,
-        _______      , TT(_ARROWS), _______, TT(_NUMBERS), TT(_SYMBOLSL), _______    , KC_LCTL, KC_RCTL, _______, _______, _______, _______, _______, _______,
-        _______      , TT(_GAMING), _______, TT(_WINDOWS), TT(_ARROWS), _______, _______, _______, _______, _______, _______, _______,
+        _______      , KC_LSFT    , KC_LCTL, KC_LALT     , KC_LCMD      , KC_TAB     , TO(_QWERTY) , KC_ENT , _______, _______, _______, _______, _______, _______,
+        _______      , MO(_ARROWS), _______, MO(_NUMBERS), MO(_SYMBOLSL), _______    , KC_LCTL, KC_RCTL, _______, _______, _______, _______, _______, _______,
+        _______      , _______    , _______, _______    , MO(_ARROWS), _______, _______, _______, _______, _______, _______, _______,
         OSM(MOD_LGUI), KC_ENT     , _______, _______     , _______      , _______
     ),
 
     [_LAUNCHERR] = LAYOUT_split_3x6_3_ex2(
-        _______, _______, _______, _______      , _______      , _______, KC_LCTL   , KC_ESC , LSFT(KC_TAB), KC_RCMD      , KC_RALT, KC_RCTL, KC_RSFT   , _______   ,
-        _______, _______, _______, _______      , _______      , _______, KC_LCTL   , KC_RCTL, KC_BSPC     , TT(_SYMBOLSR), _______, _______, TT(_MOUSE), TT(_MOUSE),
+        _______, _______, _______, _______      , _______      , _______, KC_LCTL   , TO(_QWERTY) , LSFT(KC_TAB), KC_RCMD      , KC_RALT, KC_RCTL, KC_RSFT   , _______   ,
+        _______, _______, _______, _______      , _______      , _______, KC_LCTL   , KC_RCTL, KC_BSPC     , MO(_SYMBOLSR), _______, _______, TT(_MOUSE), TT(_MOUSE),
         _______, _______, _______, _______      , _______      , _______, TG(_MOUSE), _______, _______     , _______      , _______, _______,
         _______, _______, _______, OSM(MOD_RGUI), OSM(MOD_RALT), _______
     ),
 
     [_NUMBERS] = LAYOUT_split_3x6_3_ex2(
-        _______, _______, _______, _______, _______, _______ , TO(_QWERTY), KC_RCTL, KC_EQUAL, KC_7, KC_8   , KC_9   , KC_ASTR, _______,
+        _______, _______, _______, _______, _______, _______ , TO(_QWERTY), TO(_QWERTY) , KC_EQUAL, KC_7, KC_8   , KC_9   , KC_ASTR, _______,
         _______, _______, _______, _______, _______, _______ , KC_LCTL    , KC_RCTL, KC_PLUS , KC_4, KC_5   , KC_6   , KC_PLUS, _______,
         _______, _______, _______, _______, _______, _______ , KC_MINUS   , KC_1   , KC_2    , KC_3, KC_BSLS, _______,
         _______, _______, _______, _______, KC_0   , KC_EQUAL
@@ -70,12 +68,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, LGUI(KC_LBRC), LGUI(KC_RBRC), _______
     ),
 
-    [_WINDOWS] = LAYOUT_split_3x6_3_ex2(
-        _______, _______, _______, _______, _______, _______, TO(_QWERTY), KC_RCTL, _______, _______     , _______           , _______           , KC_F3       , _______,
-        _______, _______, _______, _______, _______, _______, KC_LCTL    , KC_RCTL, _______, _______     , _______           , LSFT(LGUI(KC_TAB)), LGUI(KC_TAB), _______,
-        _______, _______, _______, _______, _______, _______, _______    , _______, _______, LGUI(KC_GRV), LSFT(LGUI(KC_GRV)), _______           ,
-        _______, _______, _______, _______, _______, _______
-    ),
 
     [_SYMBOLSL] = LAYOUT_split_3x6_3_ex2(
         KC_TILD , KC_EXLM , KC_AT  , KC_HASH, KC_DLR , KC_PERC, TO(_QWERTY), KC_RCTL , KC_CIRC , KC_AMPR, KC_ASTR      , KC_LPRN , KC_RPRN, KC_UNDS   ,
@@ -89,15 +81,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRAVE, KC_TILD, KC_GRV , _______, KC_PIPE, _______, KC_LCTL    , KC_RCTL , _______ , _______, _______, KC_LCBR , KC_RCBR, KC_KP_PLUS,
         _______ , KC_BSLS, _______, _______, _______, _______, _______    , KC_MINUS, KC_EQUAL, KC_LBRC, KC_RBRC, KC_MINUS,
         _______ , _______, _______, _______, _______, _______
-    ),
-
-    [_GAMING] = LAYOUT_split_3x6_3_ex2(
-        _______, _______, _______, KC_UP  , _______ , _______, TO(_QWERTY), _______ , _______, KC_U   , KC_I   , KC_O   , _______, _______,
-        _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______    , _______ , _______, KC_J   , KC_K   , KC_SCLN, _______, _______,
-        _______, _______, _______, _______, _______ , _______, _______    , KC_MINUS, _______, _______, _______, _______,
-        _______, _______, KC_UP  , _______, _______ , _______
     )
-  };
+};
 
 
 const uint16_t PROGMEM key_esc[]   = {KC_F, KC_J, COMBO_END};
@@ -109,7 +94,7 @@ combo_t key_combos[] = {
 };
 
 void keyboard_post_init_user(void) {
-    rgb_matrix_mode(RGB_MATRIX_MULTISPLASH);
+    rgb_matrix_mode(RGB_MATRIX_SOLID_COLOR);
 }
 
 // #ifdef ENCODER_MAP_ENABLE
@@ -122,37 +107,30 @@ void keyboard_post_init_user(void) {
 // #endif
 
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    uint8_t layer = get_highest_layer(layer_state | default_layer_state);
     for (uint8_t i = led_min; i < led_max; i++) {
-        switch (get_highest_layer(layer_state | default_layer_state)) {
-            case 9:
-                rgb_matrix_set_color(i, RGB_BLUE);
-                break;
-            case 8:
+        switch (layer) {
+            case _SYMBOLSR:
+            case _SYMBOLSL:
                 rgb_matrix_set_color(i, RGB_PURPLE);
                 break;
-            case 7:
-                rgb_matrix_set_color(i, RGB_PURPLE);
-                break;
-            case 6:
-                rgb_matrix_set_color(i, RGB_RED);
-                break;
-            case 5:
+            case _ARROWS:
                 rgb_matrix_set_color(i, RGB_PINK);
                 break;
-            case 4:
+            case _MOUSE:
                 rgb_matrix_set_color(i, RGB_ORANGE);
                 break;
-            case 3:
+            case _NUMBERS:
                 rgb_matrix_set_color(i, RGB_GREEN);
                 break;
-            case 2:
+            case _LAUNCHERR:
+            case _LAUNCHERL:
                 rgb_matrix_set_color(i, RGB_CYAN);
                 break;
-            case 1:
-                rgb_matrix_set_color(i, RGB_CYAN);
+            case _QWERTY:
+                rgb_matrix_set_color(i, 0, 0, 0);
                 break;
             default:
-                // rgb_matrix_set_color(i, RGB_AZURE);
                 break;
         }
     }
