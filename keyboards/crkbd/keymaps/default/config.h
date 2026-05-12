17fccc55a7d3f43b27e94dd261bb50cdf3997ca3
Derek Chin <derekwchin@gmail.com>
2026-05-12 13:07:06 -0700

    Remove LEADER_NO_TIMEOUT

--- keyboards/crkbd/keymaps/default/config.h
+++ keyboards/crkbd/keymaps/default/config.h
@@ -53,4 +53,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define LEADER_TIMEOUT 1000 // Gives you 1 full second to finish the sequence
 #define LEADER_PER_KEY_TIMING // Resets the timer after every keypress
-#define LEADER_NO_TIMEOUT
+// #define LEADER_NO_TIMEOUT
